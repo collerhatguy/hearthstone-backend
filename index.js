e554e823d3711d9498c77fa7515a28c0673510b0
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-10-07 18:48:50 -0400

    finsihed?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const useApi = require("./service")
 const helpers = require("./helpers")
-const file = require("fs")
+const fs = require("fs")
 const expansionData = require("./data.json")
 
 const server = express()
@@ -13,22 +13,23 @@ const getData = async () => {
         const array = helpers.convertToArray(data)
         const filteredArray = helpers.filterByExpansion(array)
         const filteredTwiceArray = helpers.filterByCard(filteredArray)
-        file.writeFile("./data.json", JSON.stringify(filteredTwiceArray), err => {
-            console.log(err)
-        })
-
+        fs.writeFile(
+            "./data.json", 
+            JSON.stringify(filteredTwiceArray), 
+            err => console.log(err)
+        )
     } catch(err) {
         console.log(err)
     }
 }
 
-getData()
 
 setInterval(getData, 1000 * 60 * 60 * 24)
 
 server.get("/", (req, res) => {
     res.status(200).json(expansionData)
 })
+
 
 const port = process.env.PORT || 3000
 
